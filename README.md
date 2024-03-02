e389708e676aa2771cfa7595f9545e9edacf9e89
Wen Chen <wchen@forte.io>
2024-03-02 14:35:30 -0500

    remove unnecessary util functions

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Ethers - KMS Signer
+# EthersKMS - a KMS Signer for Ethers
 
-ethers_kms is a signer library for [Ethers](https://github.com/ExWeb3/elixir_ethers) using a Key Management Service such [AWS KMS](https://aws.amazon.com/kms/) apart from the built-in [signers](https://github.com/ExWeb3/elixir_ethers/blob/main/lib/ethers/signer/local.ex) supported by Ethers.
+EthersKMS is a signer library for [Ethers](https://github.com/ExWeb3/elixir_ethers) using a Key Management Service such [AWS KMS](https://aws.amazon.com/kms/) apart from the built-in [signers](https://github.com/ExWeb3/elixir_ethers/blob/main/lib/ethers/signer/local.ex) supported by Ethers.
 
 
 ## Installation
@@ -14,6 +14,31 @@ def deps do
     {:ethers_kms, "~> 0.0.1"},
   ]
 end
+```
+
+## AWS configuration
+
+```elixir
+config :ex_aws,
+  access_key_id: CHANGEME,
+  secret_access_key: CHANGEME,
+  security_token: CHANGEME,
+  region: CHANGEME
+end
+```
+
+A [WebIdentityAdapter](https://github.com/ex-aws/ex_aws_sts?tab=readme-ov-file#using-web-identity-tokens-from-env-vars) can also be used in deployed environments.
+
+## Example
+
+```elixir
+MyERC20Token.transfer("0x[Recipient]", 1000)
+|> Ethers.send(
+  from: "0x[Sender]",
+  signer: EthersKMS.AWS.Signer,
+  # NOTE: provide a valid kms_key_id here (i.e., ECC_SECG_P256K1 key spec)
+  signer_opts: [kms_key_id: kms_key_id]
+)
 ```
 
 The complete documentation is available on [hexdocs](https://hexdocs.pm/ethers_kms).
--- test/support/fixtures/kms_fixtures.ex
+++ test/support/fixtures/kms_fixtures.ex
@@ -3,43 +3,6 @@ defmodule EthersKMS.KMSFixtures do
   This module defines fixtures for Signers.
   """
 
-  def kms_list_keys_truncated_response do
-    {:ok,
-     %{
-       "KeyCount" => 1,
-       "Keys" => [
-         %{
-           "KeyArn" =>
-             "arn:aws:kms:us-west-2:232250609916:key/0eb05eb7-faa7-4a65-8fc5-bf5615d1c73b",
-           "KeyId" => "0eb05eb7-faa7-4a65-8fc5-bf5615d1c73b"
-         }
-       ],
-       "NextMarker" =>
-         "AE0AAAACAHMAAAAJYWNjb3VudElkAHMAAAAMMjMyMjUwNjA5OTE2AHMAAAAEdGtJZABzAAAAJDBlYjA1ZWI3LWZhYTctNGE2NS04ZmM1LWJmNTYxNWQxYzczYg",
-       "Truncated" => true
-     }}
-  end
-
-  def kms_list_keys_response do
-    {:ok,
-     %{
-       "KeyCount" => 2,
-       "Keys" => [
-         %{
-           "KeyArn" =>
-             "arn:aws:kms:us-west-2:232250609916:key/0eb05eb7-faa7-4a65-8fc5-bf5615d1c73b",
-           "KeyId" => "0eb05eb7-faa7-4a65-8fc5-bf5615d1c73b"
-         },
-         %{
-           "KeyArn" =>
-             "arn:aws:kms:us-west-2:232250609916:key/fc038303-ea86-43fb-b277-265398349e6e",
-           "KeyId" => "fc038303-ea86-43fb-b277-265398349e6e"
-         }
-       ],
-       "Truncated" => false
-     }}
-  end
-
   def kms_public_key_response do
     {:ok,
      %{
@@ -54,30 +17,5 @@ defmodule EthersKMS.KMSFixtures do
        "Signature" =>
          "MEUCIDK6M5izIjRFuFiEniddbbsaZwjzBbsrjEJxQ/kjm7m+AiEArAX1ZsnrEthrafd+uH2PeEQ+VAN08CH/pxzG/UV2KFw="
      }}
-  end
-
-  def kms_key_creation_response do
-    {:ok,
-     %{
-       "KeyMetadata" => %{
-         "KeyId" => "c3d230fb-b257-482a-9c84-a3962b71debc"
-       }
-     }}
-  end
-
-  def kms_key_alias_response do
-    {:ok, nil}
-  end
-
-  def kms_key_disable_response do
-    {:ok, nil}
-  end
-
-  def kms_key_disable_not_found_response(key_id) do
-    {:error, {"NotFoundException", "Invalid keyId #{key_id}"}}
-  end
-
-  def kms_key_enable_response do
-    {:ok, nil}
   end
 end
